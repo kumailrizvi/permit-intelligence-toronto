ebe218a36738e66db410c93ee185b0878ccb421f
Kumail Rizvi <kumail149@hotmail.com>
2025-08-04 00:49:22 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def load_permit_data():
-    url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/1cb6f88e-0ac2-4f61-b8b1-04b3a1f1f8ff/resource/26b6cf9d-f10e-4454-993e-6247b3c8a52b/download/issued-building-permits.csv"
-    
+    url = "https://opendata.toronto.ca/building-permits/issued-building-permits.csv"
+
     try:
         df = pd.read_csv(url)
         df = df[["permit_type", "work_type", "application_date", "address", "description"]]
